b4df04f9d04487a580585466bea124444c5faf2a
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-01-28 16:35:14 -0500

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
-var randomEl = document.querySelector();
-var generate = document.addEventListener(click, randomMeal);
+var randomEl = document.querySelector("#randomBtn");
+var generate = randomEl.addEventListener(click, randomMeal);
+var listEl = document.createElement("ol");
+
 var randomMeal = function() {
-
-}
-
-// fetch random meal api
-fetch("www.themealdb.com/api/json/v1/1/random.php")
-    .then(function(response) {
-        if(response.ok) {
-            console.log(response);
-            response.json()
-                .then(function(data) {
-                    console.log(data);
-                });
+    // fetch random meal api
+    fetch("www.themealdb.com/api/json/v1/1/random.php")
+        .then(function(response) {
+            if(response.ok) {
+                console.log("response", response);
+                response.json()
+                    .then(function(data) {
+                    console.log("data", data);
+            });
         }
     });
+    // select data to show on page
+    for (i = 9; i < 20; i++) {
+    
+        if (data[i] !== "") {
+            var ingredientEl = document.createElement("li");
+            ingredientEl.textContent = data[i] + ' : ' + data[i + 10];
+            listEl.appendChild(ingredientEl);
+
+        }
+        
+        else {
+            break;
+    
+         } 
+    }
+}

bb9ad42dad9e78d04ddf9ebec36a4e32e82c87de
patfern <patrick.j.fernbach@gmail.com>
2022-01-31 17:16:36 -0500

    random button handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var recipeTitleEl = document.querySelector("#recipe-title");
 var ingredientsEl = document.querySelector("#ingredients");
 var instructionsEl = document.querySelector("#instructions");
+var randomRecipeBtnEl = document.querySelector("#random-btn")
 
 
 var getRandomRecipe = function() {
@@ -11,11 +12,20 @@ var getRandomRecipe = function() {
             console.log(data);
             recipeTitleEl.innerHTML = data.meals[0].strMeal;
             instructionsEl.innerHTML = "Instructions: <br> " + data.meals[0].strInstructions;
-            ingredientsEl
+            ingredientsEl.innerHTML = "Ingredients: <br>" + data.meals[0].strIngredient1 + ": " + data.meals[0].strMeasure1 + "<br>" +  data.meals[0].strIngredient2 + ": " + data.meals[0].strMeasure2 + "<br>" +  data.meals[0].strIngredient3 + ": " + data.meals[0].strMeasure3 + "<br>" + data.meals[0].strIngredient4 + ": " + data.meals[0].strMeasure4 + "<br>" +  data.meals[0].strIngredient5 + ": " + data.meals[0].strMeasure5 + "<br>" +  data.meals[0].strIngredient6 + ": " + data.meals[0].strMeasure6 + "<br>" +  data.meals[0].strIngredient7 + ": " + data.meals[0].strMeasure7 + "<br>" +  data.meals[0].strIngredient8 + ": " + data.meals[0].strMeasure8 + "<br>" +  data.meals[0].strIngredient9 + ": " + data.meals[0].strMeasure9 + "<br>" +  data.meals[0].strIngredient10 + ": " + data.meals[0].strMeasure10 + "<br>" +  data.meals[0].strIngredient11 + ": " + data.meals[0].strMeasure11 + "<br>" +  data.meals[0].strIngredient12 + ": " + data.meals[0].strMeasure12 + "<br>" +  data.meals[0].strIngredient13 + ": " + data.meals[0].strMeasure13 + "<br>" +  data.meals[0].strIngredient14 + ": " + data.meals[0].strMeasure14 + "<br>" +  data.meals[0].strIngredient15 + ": " + data.meals[0].strMeasure15 + "<br>" +  data.meals[0].strIngredient16 + ": " + data.meals[0].strMeasure16 + "<br>" +  data.meals[0].strIngredient17 + ": " + data.meals[0].strMeasure17 + "<br>" +  data.meals[0].strIngredient18 + ": " + data.meals[0].strMeasure18 + "<br>" +  data.meals[0].strIngredient19 + ": " + data.meals[0].strMeasure19 + "<br>" +  data.meals[0].strIngredient20 + ": " + data.meals[0].strMeasure20;
 
         });
     });
 
 };
 
-getRandomRecipe();
+var randomBtnHandler = function () {
+    recipeTitleEl.innerHTML = "";
+    ingredientsEl.innerHTML = "";
+    instructionsEl.innerHTML = "";
+    getRandomRecipe();
+}
+
+
+
+randomRecipeBtnEl.addEventListener("click", randomBtnHandler);

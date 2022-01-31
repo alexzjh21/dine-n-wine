0961cce58d6f14867cf809cde0e5654490ebc79e
patfern <patrick.j.fernbach@gmail.com>
2022-01-31 18:46:54 -0500

    meal image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var recipeTitleEl = document.querySelector("#recipe-title");
 var ingredientsEl = document.querySelector("#ingredients");
 var instructionsEl = document.querySelector("#instructions");
-var randomRecipeBtnEl = document.querySelector("#random-btn")
+var randomRecipeBtnEl = document.querySelector("#random-btn");
+var mealImgEl = document.querySelector("#meal-img");
 
 
 var getRandomRecipe = function() {
@@ -13,6 +14,9 @@ var getRandomRecipe = function() {
             recipeTitleEl.innerHTML = data.meals[0].strMeal;
             instructionsEl.innerHTML = "Instructions: <br> " + data.meals[0].strInstructions;
             ingredientsEl.innerHTML = "Ingredients: <br>" + data.meals[0].strIngredient1 + ": " + data.meals[0].strMeasure1 + "<br>" +  data.meals[0].strIngredient2 + ": " + data.meals[0].strMeasure2 + "<br>" +  data.meals[0].strIngredient3 + ": " + data.meals[0].strMeasure3 + "<br>" + data.meals[0].strIngredient4 + ": " + data.meals[0].strMeasure4 + "<br>" +  data.meals[0].strIngredient5 + ": " + data.meals[0].strMeasure5 + "<br>" +  data.meals[0].strIngredient6 + ": " + data.meals[0].strMeasure6 + "<br>" +  data.meals[0].strIngredient7 + ": " + data.meals[0].strMeasure7 + "<br>" +  data.meals[0].strIngredient8 + ": " + data.meals[0].strMeasure8 + "<br>" +  data.meals[0].strIngredient9 + ": " + data.meals[0].strMeasure9 + "<br>" +  data.meals[0].strIngredient10 + ": " + data.meals[0].strMeasure10 + "<br>" +  data.meals[0].strIngredient11 + ": " + data.meals[0].strMeasure11 + "<br>" +  data.meals[0].strIngredient12 + ": " + data.meals[0].strMeasure12 + "<br>" +  data.meals[0].strIngredient13 + ": " + data.meals[0].strMeasure13 + "<br>" +  data.meals[0].strIngredient14 + ": " + data.meals[0].strMeasure14 + "<br>" +  data.meals[0].strIngredient15 + ": " + data.meals[0].strMeasure15 + "<br>" +  data.meals[0].strIngredient16 + ": " + data.meals[0].strMeasure16 + "<br>" +  data.meals[0].strIngredient17 + ": " + data.meals[0].strMeasure17 + "<br>" +  data.meals[0].strIngredient18 + ": " + data.meals[0].strMeasure18 + "<br>" +  data.meals[0].strIngredient19 + ": " + data.meals[0].strMeasure19 + "<br>" +  data.meals[0].strIngredient20 + ": " + data.meals[0].strMeasure20;
+            var imgSrc = data.meals[0].strMealThumb;
+            mealImgEl.setAttribute('src', imgSrc+"/preview")
+
 
         });
     });
